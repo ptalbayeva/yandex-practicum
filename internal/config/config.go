6b9b98b4bd26e0382e0c32ce0bc9da8bbc38434c
talbayeva <talbayeva@kolesa.kz>
2025-12-21 14:38:32 +0500

    iter14 Добавление авторизации; правка тестов

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,8 @@ import (
 	"os"
 )
 
+type UserIDCtx struct{}
+
 type Config struct {
 	Address         string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
